6d6c17dbb242a095e972a716446c23ae59af8856
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-03-09 15:10:30 -0300

    add: container component and styles

---
+++ frontend/components/Container/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import style from './styles.module.css'
+import Link from 'next/link'
+
+type ContainerProps = {
+  title: string
+  children: React.ReactNode[] | React.ReactNode
+}
+
+export default function Container({ title, children }: ContainerProps) {
+  return (
+    <>
+      <nav className={style.navbar}>
+        <Link href="/">ENS Explorer</Link>
+      </nav>
+      <main className={style.mainContainer}>
+        <section>
+          <h2>{title}</h2>
+          {children}
+        </section>
+      </main>
+    </>
+  )
+}
