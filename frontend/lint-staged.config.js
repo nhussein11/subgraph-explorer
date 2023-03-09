4b748433cfca7abde526917b9486749ac5e582e2
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-03-09 11:26:12 -0300

    add: lint-staged setup

---
+++ frontend/lint-staged.config.js
@@ -0,0 +1,15 @@
+// lint-staged.config.js
+export default {
+  // Type check TypeScript files
+  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
+
+  // Lint then format TypeScript and JavaScript files
+  '**/*.(ts|tsx|js)': (filenames) => [
+    `yarn eslint --fix ${filenames.join(' ')}`,
+    `yarn prettier --write ${filenames.join(' ')}`,
+  ],
+
+  // Format MarkDown and JSON
+  '**/*.(md|json)': (filenames) =>
+    `yarn prettier --write ${filenames.join(' ')}`,
+}
