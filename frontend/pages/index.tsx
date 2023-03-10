41a6c31b7d74a70537fc41079f37f07530630837
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-03-10 10:56:28 -0300

    add: component path alias

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,14 @@
 import Head from 'next/head'
-import Container from '@/components/Container'
+import Container from '@components/Container'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ENS Explorer</title>
+        <title> ENS Explorer </title>
+        <meta name="description" content="ENS Explorer" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Container title="ENS Explorer">
         <p>Home page</p>
